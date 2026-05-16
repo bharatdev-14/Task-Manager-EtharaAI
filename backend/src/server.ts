ae1fcfcb1cfb86e1cd62b38021a067f99e4e0fa7
Bharat Yadav <r97940298@gmail.com>
2026-05-16 16:16:06 +0000

    update server.ts

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,10 +1,20 @@
 import { app } from "./app";
 import { env } from "./config/env";
 import { prisma } from "./config/prisma";
+import cors from "cors";
+const PORT = process.env.PORT || 5000;
 
-const server = app.listen(env.PORT, () => {
-  console.log(`API server running on port ${env.PORT}`);
+app.use(
+  cors({
+    origin: process.env.FRONTEND_URL,
+    credentials: true,
+  })
+);
+
+const server = app.listen(PORT, () => {
+  console.log(`API server running on port ${PORT}`);
 });
+
 
 const shutdown = async () => {
   console.log("Shutting down API server");
